a63cbfe7fe6bc74d79417cabee5d816285f22fb2
Justin Bedard <jb@malachiarts.com>
2019-02-22 16:40:00 -0500

    - added conditionals to prometheus.yml. - updated syntax in alertmanager.yml. - replaced hardcoded path with variable in proxy.yml. - fix file extension in permission_test.sh - added all command line parameters to run_prometheus.sh.

--- bin/permission_test.sh
+++ bin/permission_test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 APISERVER="https://tycho-api-elb-1459645966.us-west-2.elb.amazonaws.com:6443"
-CERTS="--key certs/prometheus.key --cacert certs/ca.pem --cert certs/prometheus.csr"
+CERTS="--key certs/prometheus.key --cacert certs/ca.pem --cert certs/prometheus.crt"
 # curl -v -v -s ${APISERVER}/api/v1/namespaces/default/pods/guestbook-frontend-845d478878-267dc/log $CERTS
 # curl -v -v -s ${APISERVER}/api/v1/nodes/ip-10-77-1-121.us-west-2.compute.internal/proxy/metrics $CERTS
 
--- bin/run_prometheus.sh
+++ bin/run_prometheus.sh
@@ -4,6 +4,8 @@ sudo docker run -p 9090:9090 \
 	--mount type=bind,source=/etc/prometheus,target=/etc/prometheus \
 	--mount type=bind,source=/prometheus,target=/prometheus \
 	--mount type=bind,source=/var/run/secrets/kubernetes.io/serviceaccount,target=/var/run/secrets/kubernetes.io/serviceaccount \
-	--network prom-network \
 	-d prom/prometheus \
-	--config.file=/etc/prometheus/prometheus.yml
+	--config.file=/etc/prometheus/prometheus.yml \
+  --storage.tsdb.path=/prometheus \
+  --web.route-prefix=/ \
+  --web.external-url=https://prometheus.majustfortesting.com
